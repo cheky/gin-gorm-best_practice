534632a4d7e7fbdcf1c50a1a806d5a895cbed15c
cheky <sofiyantobinwasduki@gmail.com>
2023-01-23 22:54:18 +0700

    Update response to lowercase

--- models/brg_kat.go
+++ models/brg_kat.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Brg_kat struct {
-	Kd_kat    int       `gorm:"type:int(5);primaryKey;autoIncrement"`
-	Nm_kat    string    `gorm:"type:varchar(100);not null"`
-	Aktif     string    `gorm:"type:enum('Y','T');default:Y"`
-	On_create time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP()"`
-	On_update time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP"`
+	Kd_kat    int       `gorm:"type:int(5);primaryKey;autoIncrement" json:"kd_kat"`
+	Nm_kat    string    `gorm:"type:varchar(100);not null" json:"nm_kat"`
+	Aktif     string    `gorm:"type:enum('Y','T');default:Y" json:"aktif"`
+	On_create time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP()" json:"on_create"`
+	On_update time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP" json:"on_update"`
 }
 
 type Tabler interface {
